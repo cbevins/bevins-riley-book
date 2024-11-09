2b84930c5df479247ff3493a5e7d8f8fb6e0bceb
Collin D Bevins <cbevins@montana.com>
2024-11-09 12:47:36 -0700

    ADded links to ToC

--- src/routes/story-book/MyToC.svelte
+++ src/routes/story-book/MyToC.svelte
@@ -2,15 +2,26 @@
     import { itemSequence } from '$lib/book'
     export let man
 
-    function href(item) { return itemSequence(item, '-', 0, '#book-') }
-
-    function seq(item) { return itemSequence(item, '.')}
+    function href(item) {
+        if (item.type === 'section')
+            return itemSequence(item, '-', 0, '#book-')
+        if (item.type === 'figure')
+            return '#book-figure-' + item.id
+        if (item.type === 'table')
+            return '#book-table-' + item.id
+    }
 
     function indentHtml(item) {
         const spaces = '&nbsp;&nbsp;&nbsp;&nbsp;'
         let indent = ''
         for (let i=0; i<item.depth; i++) indent += spaces
         return indent
+    }
+
+    function seq(item) {
+        if (item.type === 'section') return itemSequence(item, '.')
+        if (item.type === 'figure') return `Figure ${item.id}`
+        if (item.type === 'table') return `Table ${item.id}`
     }
 </script>
 
@@ -21,10 +32,13 @@
                 <tr class="odd:bg-white even:bg-gray-50 bg-white border-b ">
                     <td class="px-2 py-1">
                         {@html indentHtml(item)}
-                        <a href={href(item)}>{seq(item)}  {item.title}</a>
+                        <a href={href(item)}>
+                            <span class="underline">{seq(item)}</span>
+                            : {item.title}
+                        </a>
                     </td>
                     <td class="px-2 py-1">
-                        <a href='#page-{item.page.pageno}'>
+                        <a class="underline" href='#page-{item.page.pageno}'>
                         {item.page.pageno}</a>
                     </td>
                 </tr>
